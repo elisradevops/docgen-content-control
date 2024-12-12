c9bc963a0320d5cd114e8433ebed2c5a03f0ffae
Eden Zvi Schwartz <eden@korentec.co.il>
2024-12-12 12:03:27 +0200

    #181 Add support for pipeline range
    - Add mapping of pipeline range data
    - Added the ability to add "No data" message

--- src/adapters/ChangesTableDataSkinAdapter.ts
+++ src/adapters/ChangesTableDataSkinAdapter.ts
@@ -14,7 +14,10 @@ export default class ChangesTableDataSkinAdapter {
     this.includeChangeDescription = includeChangeDescription;
     this.includeCommittedBy = includeCommittedBy;
     this.htmlUtils = new HtmlUtils();
-    console.log('Constructor: Initialized ChangesTableDataSkinAdapter with rawChangesArray', rawChangesArray);
+    console.log(
+      'Constructor: Initialized ChangesTableDataSkinAdapter with rawChangesArray',
+      JSON.stringify(rawChangesArray)
+    );
   }
 
   getAdoptedData() {
@@ -83,87 +86,108 @@ export default class ChangesTableDataSkinAdapter {
     console.log('adoptSkinData: Started adopting skin data');
     let i = 0;
     this.rawChangesArray.forEach((artifact) => {
-      let artifactTitle: any = [
-        {
-          fields: [{ name: 'Description', value: `Artifact name: ${artifact.artifact.name}` }],
-        },
-      ];
-      let artifactChanges: any = [];
-      artifact.changes.forEach((change) => {
-        let changeTableRow;
-        if (change.workItem) {
-          //TODO: add toggle for adding system description
-          //TODO: add toggle for adding committed by
-          const description: string = change.workItem.fields['System.Description'];
-          changeTableRow = {
-            fields: [
-              { name: '#', value: i + 1, width: '3.8%' },
-              {
-                name: 'Change #',
-                ...this.applyChangeNumber(change),
-                width: '7.6%',
-              },
-              {
-                name: 'WI ID',
-                value: `${change.workItem.id}`,
-                url: change.workItem._links.html.href,
-                width: '6.8%',
-              },
-              {
-                name: 'WI Type',
-                value: `${change.workItem.fields['System.WorkItemType']}`,
-                width: '11.9%',
-              },
-              {
-                name: 'WI Title',
-                value: `${change.workItem.fields['System.Title']}`,
-              },
-              {
-                name: 'Change description',
-                condition: this.includeChangeDescription,
-                value: description ? this.htmlUtils.cleanHtml(description) : '',
-                width: '20.8%',
-              },
-              { name: 'Committed Date & Time', ...this.applyClosedDateData(change), width: '10%' },
-              {
-                name: 'Committed by',
-                ...this.applyCommitterData(change),
-                width: '11.4%',
-                condition: this.includeCommittedBy,
-              },
-            ].filter((field) => field.condition === undefined || field.condition === true),
-          };
-        } //if include Pull Requests is True
-        else {
-          changeTableRow = {
-            fields: [
-              { name: '#', value: i + 1, width: '7.6%' },
-              {
-                name: 'Pull Request Title',
-                value: change.title,
-              },
-              {
-                name: 'Pull Request Description',
-                value: change.description,
-                condition: this.includeChangeDescription,
-              },
-              {
-                name: 'Creation date',
-                value: change.creationDate,
-                width: '10%',
-              },
-              { name: 'Created by', value: change.createdBy, condition: this.includeCommittedBy },
-            ].filter((field) => field.condition === undefined || field.condition === true),
-          };
-        }
-        artifactChanges.push(changeTableRow);
-        i++;
-      });
+      let artifactObject = {};
+      if (artifact.artifact.name !== '') {
+        let artifactTitle: any = [
+          {
+            fields: [{ name: 'Description', value: `Artifact name: ${artifact.artifact.name}` }],
+          },
+        ];
+        artifactObject['artifact'] = artifactTitle;
+      }
 
-      this.adoptedData.push({
-        artifact: artifactTitle,
-        artifactChanges: artifactChanges,
-      });
+      let artifactChanges: any = [];
+      if (artifact.changes?.length > 0) {
+        artifact.changes?.forEach((change) => {
+          let changeTableRow;
+          if (change.workItem) {
+            //TODO: add toggle for adding system description
+            //TODO: add toggle for adding committed by
+            const description: string = change.workItem.fields['System.Description'];
+            changeTableRow = {
+              fields: [
+                { name: '#', value: i + 1, width: '3.8%' },
+                change.targetRepo
+                  ? {
+                      name: 'Repository',
+                      value: change.targetRepo.repoName,
+                      url: change.targetRepo.url,
+                      width: '9.8%',
+                    }
+                  : null,
+                {
+                  name: 'Change #',
+                  ...this.applyChangeNumber(change),
+                  width: '7.6%',
+                },
+                {
+                  name: 'WI ID',
+                  value: `${change.workItem.id}`,
+                  url: change.workItem._links.html.href,
+                  width: '6.8%',
+                },
+                {
+                  name: 'WI Type',
+                  value: `${change.workItem.fields['System.WorkItemType']}`,
+                  width: '11.9%',
+                },
+                {
+                  name: 'WI Title',
+                  value: `${change.workItem.fields['System.Title']}`,
+                },
+                {
+                  name: 'Change description',
+                  condition: this.includeChangeDescription,
+                  value: description ? this.htmlUtils.cleanHtml(description) : '',
+                  width: '20.8%',
+                },
+                { name: 'Committed Date & Time', ...this.applyClosedDateData(change), width: '10%' },
+                {
+                  name: 'Committed by',
+                  ...this.applyCommitterData(change),
+                  width: '11.4%',
+                  condition: this.includeCommittedBy,
+                },
+              ]
+                .filter((field) => field !== null) // Remove null fields
+                .filter((field) => field.condition === undefined || field.condition === true), // Filter by condition
+            };
+          } //if include Pull Requests is True
+          else {
+            changeTableRow = {
+              fields: [
+                { name: '#', value: i + 1, width: '7.6%' },
+                {
+                  name: 'Pull Request Title',
+                  value: change.title,
+                },
+                {
+                  name: 'Pull Request Description',
+                  value: change.description,
+                  condition: this.includeChangeDescription,
+                },
+                {
+                  name: 'Creation date',
+                  value: change.creationDate,
+                  width: '10%',
+                },
+                { name: 'Created by', value: change.createdBy, condition: this.includeCommittedBy },
+              ].filter((field) => field.condition === undefined || field.condition === true),
+            };
+          }
+          artifactChanges.push(changeTableRow);
+          i++;
+        });
+        artifactObject['artifactChanges'] = artifactChanges;
+      } else {
+        artifactObject['errorMessage'] = [
+          {
+            fields: [{ name: 'Title', value: 'No data found for the requested parameters' }],
+          },
+        ];
+      }
+
+      this.adoptedData.push(artifactObject);
     });
 
     console.log('adoptSkinData: Completed adopting skin data', this.adoptedData);
--- src/factories/ChangeDataFactory.ts
+++ src/factories/ChangeDataFactory.ts
@@ -61,129 +61,227 @@ export default class ChangeDataFactory {
 
   /*fetches Change table data and adopts it to json skin format */
   async fetchData() {
-    let focusedArtifact;
-    let artifactChanges;
-    let origin;
-    let gitDataProvider = await this.dgDataProviderAzureDevOps.getGitDataProvider();
-    let pipelinesDataProvider = await this.dgDataProviderAzureDevOps.getPipelinesDataProvider();
-    if (this.repoId) {
-      focusedArtifact = await gitDataProvider.GetGitRepoFromRepoId(this.repoId);
-    }
-    switch (this.rangeType) {
-      case 'commitSha':
-        let commitsInCommitRange = await gitDataProvider.GetCommitsInCommitRange(
-          this.teamProject,
-          this.repoId,
-          String(this.to),
-          String(this.from)
-        );
-        artifactChanges = await gitDataProvider.GetItemsInCommitRange(
-          this.teamProject,
-          this.repoId,
-          commitsInCommitRange
-        );
-        this.rawChangesArray.push({
-          artifact: focusedArtifact,
-          changes: artifactChanges,
-        });
-        break;
-      case 'date':
-        let commitsInDateRange = await gitDataProvider.GetCommitsInDateRange(
-          this.teamProject,
-          this.repoId,
-          String(this.from),
-          String(this.to),
-          this.branchName
-        );
-
-        if (this.includePullRequests) {
-          console.log(this.includePullRequests);
-          artifactChanges = await gitDataProvider.GetPullRequestsInCommitRangeWithoutLinkedItems(
+    try {
+      let focusedArtifact;
+      let artifactChanges;
+      let origin;
+      let gitDataProvider = await this.dgDataProviderAzureDevOps.getGitDataProvider();
+      let pipelinesDataProvider = await this.dgDataProviderAzureDevOps.getPipelinesDataProvider();
+      if (this.repoId) {
+        focusedArtifact = await gitDataProvider.GetGitRepoFromRepoId(this.repoId);
+      }
+      switch (this.rangeType) {
+        case 'commitSha':
+          let commitsInCommitRange = await gitDataProvider.GetCommitsInCommitRange(
             this.teamProject,
             this.repoId,
-            commitsInDateRange
+            String(this.to),
+            String(this.from)
           );
-        } else {
-          console.log(this.includePullRequests);
           artifactChanges = await gitDataProvider.GetItemsInCommitRange(
             this.teamProject,
             this.repoId,
-            commitsInDateRange
+            commitsInCommitRange
           );
-        }
-        this.rawChangesArray.push({
-          artifact: focusedArtifact,
-          changes: artifactChanges,
-        });
-        break;
+          this.rawChangesArray.push({
+            artifact: focusedArtifact,
+            changes: artifactChanges,
+          });
+          break;
+        case 'date':
+          let commitsInDateRange = await gitDataProvider.GetCommitsInDateRange(
+            this.teamProject,
+            this.repoId,
+            String(this.from),
+            String(this.to),
+            this.branchName
+          );
 
-      case 'pipeline':
-        focusedArtifact = await pipelinesDataProvider.getPipelineFromPipelineId(
-          this.teamProject,
-          Number(this.to)
-        );
-        artifactChanges = await gitDataProvider.GetItemsForPipelinesRange(
-          this.teamProject,
-          Number(this.from),
-          Number(this.to)
-        );
-        this.rawChangesArray.push({
-          artifact: { name: focusedArtifact.repository.name },
-          changes: artifactChanges,
-        });
-        break;
-      case 'release':
-        //get list of artifacts for each release
-        let fromRelease = await pipelinesDataProvider.GetReleaseByReleaseId(
-          this.teamProject,
-          Number(this.from)
-        );
-        let toRelease = await pipelinesDataProvider.GetReleaseByReleaseId(this.teamProject, Number(this.to));
-        logger.info(`retrived release artifacts for releases : ${fromRelease} - ${toRelease}`);
-        //create factory for each aritfact
-        await Promise.all(
-          toRelease.artifacts.map(async (toArtifact) => {
-            let fromArtifact = fromRelease.artifacts.filter(
-              (artifact) => artifact.sourceId === toArtifact.sourceId
+          if (this.includePullRequests) {
+            console.log(this.includePullRequests);
+            artifactChanges = await gitDataProvider.GetPullRequestsInCommitRangeWithoutLinkedItems(
+              this.teamProject,
+              this.repoId,
+              commitsInDateRange
             );
-            //checks if artifacts did not exist in previos release
-            fromArtifact = fromArtifact[0] || null;
-            if (fromArtifact == null) {
-              fromArtifact = toArtifact;
-            }
-            switch (toArtifact.type) {
-              case 'Build':
-                logger.debug(
-                  `fetching links between ${fromArtifact.definitionReference.version.id}-${toArtifact.definitionReference.version.id}`
-                );
-                let buildChangeFactory = new ChangeDataFactory(
-                  this.teamProject,
-                  fromArtifact.definitionReference.repository.name,
-                  fromArtifact.definitionReference.version.id,
-                  toArtifact.definitionReference.version.id,
-                  'pipeline',
-                  null,
-                  '', // You can provide the appropriate branch name here or an empty string if not applicable
-                  true,
-                  false,
-                  false,
-                  this.dgDataProviderAzureDevOps
-                );
+          } else {
+            console.log(this.includePullRequests);
+            artifactChanges = await gitDataProvider.GetItemsInCommitRange(
+              this.teamProject,
+              this.repoId,
+              commitsInDateRange
+            );
+          }
+          this.rawChangesArray.push({
+            artifact: focusedArtifact,
+            changes: artifactChanges,
+          });
+          break;
 
-                await buildChangeFactory.fetchData();
-                let rawData = buildChangeFactory.getRawData();
-                this.rawChangesArray = [...this.rawChangesArray, ...rawData];
-                break;
-              case 'Git':
-                break;
+        case 'pipeline':
+          artifactChanges = [];
+          let targetBuild = await pipelinesDataProvider.getPipelineFromPipelineId(
+            this.teamProject,
+            Number(this.to)
+          );
+
+          if (targetBuild.result !== 'succeeded') {
+            throw new Error(`The selected ${this.to} build has not been succeeded`);
+          }
+
+          let targetPipelineId = targetBuild.definition.id;
+          let sourceBuild = await pipelinesDataProvider.getPipelineFromPipelineId(
+            this.teamProject,
+            Number(this.from)
+          );
+          //Currently not needed
+          // if (sourceBuild.result !== 'succeeded') {
+          //   //Finding previous successful build
+          //   let foundSourcePipelineId = await pipelinesDataProvider.findPreviousPipeline(
+          //     this.teamProject,
+          //     targetBuild.id,
+          //     Number(this.to),
+          //     targetBuild,
+          //     true
+          //   );
+          //   if (!foundSourcePipelineId) {
+          //     throw new Error(`Could not find a valid pipeline before build #${this.to}`);
+          //   }
+          //   this.from = Number(foundSourcePipelineId);
+          //   sourceBuild = await pipelinesDataProvider.getPipelineFromPipelineId(
+          //     this.teamProject,
+          //     foundSourcePipelineId
+          //   );
+          // }
+
+          let sourcePipelineId = sourceBuild.definition.id;
+
+          let sourcePipelineRun = await pipelinesDataProvider.getPipelineRunBuildById(
+            this.teamProject,
+            sourcePipelineId,
+            Number(this.from)
+          );
+
+          let targetPipelineRun = await pipelinesDataProvider.getPipelineRunBuildById(
+            this.teamProject,
+            targetPipelineId,
+            Number(this.to)
+          );
+
+          const sourceResourceRepositories =
+            await pipelinesDataProvider.getPipelineResourceRepositoriesFromObject(
+              sourcePipelineRun,
+              gitDataProvider
+            );
+          const targetResourceRepositories =
+            await pipelinesDataProvider.getPipelineResourceRepositoriesFromObject(
+              targetPipelineRun,
+              gitDataProvider
+            );
+
+          for (const targetPipelineRepo of targetResourceRepositories) {
+            let fromPipelineRepoFound = false;
+            let gitRepoUrl = targetPipelineRepo.url;
+            let gitRepoVersion = targetPipelineRepo.repoSha1;
+            let gitRepoName = targetPipelineRepo.repoName;
+            let toCommit = gitRepoVersion;
+            logger.debug(`Repository ${gitRepoUrl} version ${gitRepoVersion.slice(0, 7)}`);
+            for (const sourcePipeline of sourceResourceRepositories) {
+              let fromGitRepoUrl = sourcePipeline.url;
+              let fromGitRepoVersion = sourcePipeline.repoSha1;
+              let fromGitRepoName = sourcePipeline.repoName;
+
+              if (fromGitRepoName !== gitRepoName) {
+                continue;
+              }
+
+              logger.debug(`Previous repository ${fromGitRepoUrl} version ${fromGitRepoVersion.slice(0, 7)}`);
+              fromPipelineRepoFound = true;
+              if (fromGitRepoVersion === gitRepoVersion) {
+                logger.debug(`Same repository version ${fromGitRepoVersion} nothing to compare`);
+              }
+
+              let fromCommit = fromGitRepoVersion;
+              let repoId = fromGitRepoUrl.split('/').pop();
+              let extendedCommits = await gitDataProvider.GetCommitBatch(
+                this.teamProject,
+                repoId,
+                { version: fromCommit, versionType: 'commit' },
+                { version: toCommit, versionType: 'commit' }
+              );
+              if (extendedCommits?.length > 0) {
+                const pipelineRangeItems = await gitDataProvider.getItemsForPipelineRange(
+                  this.teamProject,
+                  extendedCommits,
+                  { repoName: gitRepoName, url: gitRepoUrl }
+                );
+                artifactChanges.push(...pipelineRangeItems);
+              }
             }
-          })
-        );
-        break;
-      default:
-        break;
+          }
+          this.rawChangesArray.push({
+            artifact: { name: '' },
+            changes: artifactChanges,
+          });
+          break;
+        case 'release':
+          //get list of artifacts for each release
+          let fromRelease = await pipelinesDataProvider.GetReleaseByReleaseId(
+            this.teamProject,
+            Number(this.from)
+          );
+          let toRelease = await pipelinesDataProvider.GetReleaseByReleaseId(
+            this.teamProject,
+            Number(this.to)
+          );
+          logger.info(`retrived release artifacts for releases : ${fromRelease} - ${toRelease}`);
+          //create factory for each aritfact
+          await Promise.all(
+            toRelease.artifacts.map(async (toArtifact) => {
+              let fromArtifact = fromRelease.artifacts.filter(
+                (artifact) => artifact.sourceId === toArtifact.sourceId
+              );
+              //checks if artifacts did not exist in previos release
+              fromArtifact = fromArtifact[0] || null;
+              if (fromArtifact == null) {
+                fromArtifact = toArtifact;
+              }
+              switch (toArtifact.type) {
+                case 'Build':
+                  logger.debug(
+                    `fetching links between ${fromArtifact.definitionReference.version.id}-${toArtifact.definitionReference.version.id}`
+                  );
+                  let buildChangeFactory = new ChangeDataFactory(
+                    this.teamProject,
+                    fromArtifact.definitionReference.repository.name,
+                    fromArtifact.definitionReference.version.id,
+                    toArtifact.definitionReference.version.id,
+                    'pipeline',
+                    null,
+                    '', // You can provide the appropriate branch name here or an empty string if not applicable
+                    true,
+                    false,
+                    false,
+                    this.dgDataProviderAzureDevOps
+                  );
+
+                  await buildChangeFactory.fetchData();
+                  let rawData = buildChangeFactory.getRawData();
+                  this.rawChangesArray = [...this.rawChangesArray, ...rawData];
+                  break;
+                case 'Git':
+                  break;
+              }
+            })
+          );
+          break;
+        default:
+          break;
+      }
+      logger.info(`fetch ${this.rawChangesArray.length} changes for range`);
+    } catch (error: any) {
+      logger.error(error.message);
     }
-    logger.info(`fetch ${this.rawChangesArray.length} changes for range`);
   } //fetchData
 
   /*arranging the test data for json skins package*/
